30b971fc5570a0bf7daf34f97ad757469cd3f564
darren.gong <913060979@qq.com>
2015-11-21 11:17:34 +0800

    一些元素

--- LingCaiWang/src/com/bgf/lingcaiwang/ui/MainActivity.java
+++ LingCaiWang/src/com/bgf/lingcaiwang/ui/MainActivity.java
@@ -1,35 +1,25 @@
 package com.bgf.lingcaiwang.ui;
 
 import com.bgf.lingcaiwang.R;
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
+	
+	private static final String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initViews();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private void initViews() {
+		
+		ImageButton mIbBack = (ImageButton) findViewById(R.id.header_back_ibtn);
+		mIbBack.setVisibility(View.GONE);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
